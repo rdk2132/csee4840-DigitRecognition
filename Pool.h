ec25eeb0f7fe9e3c4d5ae9fb1b6e2538d3b04605
Felix Hanau <hanau2@illinois.edu>
2022-03-18 20:56:54 -0400

    Add pooling layer – depends on a number of higher-level design decisions

---
+++ Pool.h
@@ -0,0 +1,45 @@
+#ifndef POOL_H
+#define POOL_H
+#include "Parameters.h"
+//TODO: Maybe we'll switch to just integers, but plan with fixed point for
+//now. This header will need to define the fixed point type and some
+//functions. Otherwise just use int or perhaps unsigned char types.
+#include "FixedPoint.h"
+
+//Implement 2x2 pooling for now – different pooling or adding padding is
+//not difficult to do
+#define POOL_SIZE 2
+
+//This will compile and work correctly as long as fixed_t is an alias for
+//an integer type
+#define MAX(a, b) (a > b ? a : b)
+//TODO: Is pooling 3D or just some of the other layers? Switch to
+//fixed_t** in that case and add outer loop for layers
+//TODO: Could return the output if memory allocation is allowed in
+//here, handle it elsewhere for now.
+void max_pool(fixed_t* in, fixed_t* out) {
+  //DATA_WIDTH_POOL needs to be defined in Parameters.h
+  const int out_width = DATA_WIDTH_POOL / POOL_SIZE;
+  for (int i = 0; i < out_width; i++) {
+    for (int j = 0; j < out_width; j++) {
+      fixed_t max = 0;
+      //Helps de-clutter the in array access
+      int start_idx = POOL_SIZE * (i * DATA_WIDTH_POOL + j);
+      for (int k = 0; k < POOL_SIZE; k++) {
+        for (int l = 0; l < POOL_SIZE; l++) {
+          max = MAX(max, in[start_idx + k * DATA_WIDTH_POOL + l]);
+        }
+      }
+      out[i * out_width + j] = max;
+    }
+  }
+}
+
+//TODO: Could also develop a struct based interface instead where
+//parameters are passed to struct, e.g.
+//Pool layer4(3);
+//...
+//fixed_t* pooled_data = layer4.process(relu_data);
+
+#endif
+
