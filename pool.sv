3091bbaf1dc0a324891904f07c20653110406359
Felix Hanau <hanau2@illinois.edu>
2022-04-06 13:02:18 -0400

    avg shift update

--- pool.sv
+++ pool.sv
@@ -8,6 +8,7 @@ module avg2x2
    output logic signed [WORD_SIZE - 1:0] poolavg);
   //TODO: Right shifting negative number may or may not work
   //assign poolavg = (a0 + a1 + b0 + b1) >> 2;
+  //TODO: This may or may not work due to two's complement, arithmetic right shift >>> should work
   logic signed [WORD_SIZE - 1:0] imm;
   assign imm = (a0 + a1 + b0 + b1);
   assign poolavg = {imm[WORD_SIZE - 1], 2'b0, imm[WORD_SIZE - 4 : 0]};
