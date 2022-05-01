2e485c7d215979c585a7377cd3499d79e6497529
Felix Hanau <hanau2@illinois.edu>
2022-05-01 15:15:00 -0400

    Implement conv1 input counters, fix pooling coounters

--- hardware/mem_counters.sv
+++ hardware/mem_counters.sv
@@ -38,12 +38,16 @@ module img_mem_write (input logic clk, reset, enable, next,
 
 endmodule
 
+//img_mem_read is responsible for one output image and uses four output addresses in parallel. Each processes a quarter of the image, the offset between addresses is 28 * 24 / 4 = 168
 // counter/addresser for input image memory read
 module img_mem_read (input logic clk, reset, enable,
                      output reg [9:0] addr0, addr1, addr2, addr3, 
                      output logic done);
 
+    //kernel row and column count
     logic [2:0] rowcount, columncount;
+    //counter for position within image row
+    logic [4:0] i_count;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
@@ -51,14 +55,51 @@ module img_mem_read (input logic clk, reset, enable,
             addr1 <= 10'b0010101000;
             addr2 <= 10'b0101010000;
             addr3 <= 10'b0111111000;
+            i_count <= 5'b0;
         end
         else if (enable == 1'b1 && done == 1'b0) begin
-
+            if (i_count == 5'b10111 && rowcount == 3'b100 && columncount == 3'b100) begin
+                //done with applying the kernel over an entire image row, go to the next row
+                //Move addresses 4 rows up, 27 pixels back and 1 row down, i.e. -111
+                i_count <= 5'b0;
+                columncount <= 3'b0;
+                rowcount <= 3'b0;
+                addr0 <= addr0 - 10'b0001101111;
+                addr1 <= addr1 - 10'b0001101111;
+                addr2 <= addr2 - 10'b0001101111;
+                addr3 <= addr3 - 10'b0001101111;
+            end
+            else if (rowcount == 3'b100 && columncount == 3'b100) begin
+                //done with a position for the kernel, move forward to the next position. Move 4 rows up, 4 pixels back and 1 pixel forward, i.e. -115
+                columncount <= 3'b0;
+                rowcount <= 3'b0;
+                addr0 <= addr0 - 10'b0001110011;
+                addr1 <= addr1 - 10'b0001110011;
+                addr2 <= addr2 - 10'b0001110011;
+                addr3 <= addr3 - 10'b0001110011;
+                i_count <= i_count + 1;
+            end
+            else if (columncount == 3'b100) begin
+                //kernel row has been processed, move addresses one row down and 4 pixels back, i.e. + 24
+                columncount <= 3'b0;
+                rowcount <= rowcount + 1;
+                addr0 <= addr0 + 10'b0000011000;
+                addr1 <= addr1 + 10'b0000011000;
+                addr2 <= addr2 + 10'b0000011000;
+                addr3 <= addr3 + 10'b0000011000;
+            end
+            else begin
+                addr0 <= addr0 + 1;
+                addr1 <= addr1 + 1;
+                addr2 <= addr2 + 1;
+                addr3 <= addr3 + 1;
+                columncount <= columncount + 1;
+            end
         end
     end
 
     always_comb begin
-        if(addr3 == 10'b1100010000) begin
+        if(addr3 == 10'b1100001111) begin
             done = 1'b1;
         end
     end
@@ -76,8 +117,8 @@ module conv1_k_mem_read (input logic clk, reset, enable,
             addr1 <= 6'b011001;
         end
         else if (enable == 1'b1 && done == 1'b0) begin
-            addr0 <= addr1 + 1;
-            addr1 <= addr2 + 1;
+            addr0 <= addr0 + 1;
+            addr1 <= addr1 + 1;
         end
     end
 
@@ -129,7 +170,7 @@ module conv1_mem_read (input logic clk, reset, enable,
         end
         
         else if (enable == 1'b1 && done == 1'b0) begin
-            if(count == 4'b1100) begin
+            if(count == 4'b1011) begin
                 count <= 4'b0000;
                 addr0 <= addr0 + 26;
                 addr1 <= addr1 + 26;
@@ -255,19 +296,19 @@ module conv2_mem_read (input logic clk, reset, enable,
                         output reg [5:0] addr0, addr1, addr2, addr3,
                         output logic done);
 
-    reg [2:0] count;
+    reg [1:0] count;
     always_ff @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
             addr0 <= 6'b000000;
             addr1 <= 6'b000001;
             addr2 <= 6'b001000;
             addr3 <= 6'b001001;
-            count <= 3'b000;
+            count <= 2'b00;
         end
 
         else if (enable == 1'b1 && done == 1'b0) begin
-            if(count == 3'b100) begin
-                count <= 3'b000;
+            if(count == 2'b11) begin
+                count <= 2'b00;
                 addr0 <= addr0 + 10;
                 addr1 <= addr1 + 10;
                 addr2 <= addr2 + 10;
