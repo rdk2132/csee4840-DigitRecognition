683f00c2aefbfcbb835fe96e111f29f34c7088da
Felix Hanau <hanau2@illinois.edu>
2022-05-03 14:33:34 -0400

    Add conv2 output counter

--- hardware/mem_counters.sv
+++ hardware/mem_counters.sv
@@ -304,23 +304,31 @@ module conv2_k_mem_read (input logic clk, reset, enable,
 
 endmodule
 
+//Go through an 8x8 output image 3 times
 // counter/addresser for Convolution 2 layer output memory write
 module conv2_mem_write (input logic clk, reset, enable,
-                        output reg [6:0] addr0, addr1
+                        output reg [5:0] addr0,
                         output logic done);
+    reg [1:0] count;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
-            addr0 <=;
-            addr1 <=;
+            addr0 <= 0;
+            count <= 0;
         end
         else if (enable == 1'b1 && done == 1'b0) begin
-
+            if(addr0 == 6'b111111) begin
+                count <= count + 1;
+                addr0 <= 0;
+            end
+            else begin
+                addr0 <= addr0 + 1;
+            end
         end
     end
     
     always_comb begin
-        if() begin
+        if(count == 2'b10 && addr0 == 6'b111111) begin
             done = 1'b1;
         end
     end
