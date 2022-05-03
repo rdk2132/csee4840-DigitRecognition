c6a7841f62e40b10a053e804c3bdb4505f4237bb
Felix Hanau <hanau2@illinois.edu>
2022-05-03 01:23:10 -0400

    Implement fc layer input counter

--- hardware/mem_counters.sv
+++ hardware/mem_counters.sv
@@ -392,23 +392,31 @@ module P2_mem_write (input logic clk, reset, enable,
 
 endmodule
 
+//cycle through 4x4 input images 12 times
 // counter/addresser for pooling 2 layer output memory read
 module P2_mem_read (input logic clk, reset, enable,
-                        output reg [4:0] addr0, addr1, addr2, addr3,
+                        output reg [3:0] addr0,
                         output logic done);
+    reg [3:0] count;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
-            addr0 <=;
-            addr1 <=;
+            count <= 0;
+            addr0 <= 0;
         end
         else if (enable == 1'b1 && done == 1'b0) begin
-
+            if(addr0 == 4'b1111) begin
+                count <= count + 1;
+                addr0 <= 0;
+            end
+            else begin
+                addr0 <= addr0 + 1;
+            end
         end
     end
-    
+
     always_comb begin
-        if() begin
+        if(count == 4'b1011 && addr0 == 4'b1111) begin
             done = 1'b1;
         end
     end
