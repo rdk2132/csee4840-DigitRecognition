1250cfbd030724c51307d265f1aa39e4fc173d26
Felix Hanau <fjh2116@columbia.edu>
2022-05-07 13:26:03 -0400

    Remove extra copies of mem counters, improve mem counter test build

--- hardware/mem_counter_test/run.sh
+++ hardware/mem_counter_test/run.sh
@@ -1,6 +1,4 @@
 mkdir obj_dir
-verilator -trace -Wall -cc $1.sv -exe $1.cpp -top-module $1;
-cd obj_dir
-make -j -f V$1.mk;
-cd ..;
+verilator -trace -Wall -cc ../mem_counters/$1.sv -exe $1.cpp -top-module $1;
+make -j -C obj_dir -f V$1.mk;
 ./obj_dir/V$1;
