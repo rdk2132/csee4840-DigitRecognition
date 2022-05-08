dc642419ab2b15bc73f6d2fcc82c8bba6d7b78d2
Liam Bishop <lb3306@columbia.edu>
2022-05-08 00:30:49 +0000

    Driver testben progress

--- kernel-software/driver_test.c
+++ kernel-software/driver_test.c
@@ -12,9 +12,9 @@ int cnn_fd;
 void run_cnn(const fixed_t *image) {
 
   cnn_arg_t cnn_data;
-  memcpy(&cnn_data, image, 25);
+  memcpy(&cnn_data, image, 50);
 
-  if (ioctl(cnn_fd, CNN_CLASSIFY, cnn_data)) {
+  if (ioctl(cnn_fd, CNN_CLASSIFY, &cnn_data)) {
     perror("ioctl(CNN_CLASSIFY) failed");
     return;
   }
@@ -25,14 +25,14 @@ void run_cnn(const fixed_t *image) {
 
 int main()
 {
-    static const char filename[] = "/dev/cnn_fpga";
+    static const char filename[] = "/dev/cnn";
     static const fixed_t image[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 
                12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
     
 
     printf("Starting device driver test\n");
 
-    if ((cnn_fd = open(filename, O_RDWR)) == -1) {
+    if ( (cnn_fd = open(filename, O_RDWR)) == -1) {
         fprintf(stderr, "Could not open %s\n", filename);
         return -1;
     }
