a28929af1ab9143e7bd2625a57911bfba970a351
Richard Mouradian <richardmouradian@Richards-MacBook-Air.local>
2022-05-09 16:02:54 -0400

    errors

--- hardware/routing/demux_1to2.sv
+++ hardware/routing/demux_1to2.sv
@@ -7,7 +7,6 @@ module demux_1to2 #(parameter WORD_SIZE = 16)
         case (sel)
             2'd0: data_out_0 = data_in;
             2'd1: data_out_1 = data_in;
-            default: data_out_0 = data_out_1 = 0;
         endcase
     end
 endmodule
