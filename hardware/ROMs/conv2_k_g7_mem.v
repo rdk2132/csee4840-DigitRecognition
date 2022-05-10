5e24dddb731a69e88a05271470771f4db6446c9c
danielcooke <dwcookejr@gmail.com>
2022-05-09 21:02:38 -0400

    fixed mem

--- hardware/ROMs/conv2_k_g7_mem.v
+++ hardware/ROMs/conv2_k_g7_mem.v
@@ -105,7 +105,7 @@ module conv2_k_g7_mem (
 		altsyncram_component.clock_enable_output_a = "BYPASS",
 		altsyncram_component.clock_enable_output_b = "BYPASS",
 		altsyncram_component.indata_reg_b = "CLOCK0",
-		altsyncram_component.init_file = "init/conv2_k_g7.mif",
+		altsyncram_component.init_file = "conv2_k_g7.mif",
 		altsyncram_component.intended_device_family = "Cyclone V",
 		altsyncram_component.lpm_type = "altsyncram",
 		altsyncram_component.numwords_a = 150,
