6840386975b660acf25a2e083b67e51a8ac8f267
danielcooke <dwcookejr@gmail.com>
2022-05-09 20:31:54 -0400

    updated fc_mem_read

--- hardware/mem_counters/fc_mem_read.sv
+++ hardware/mem_counters/fc_mem_read.sv
@@ -10,19 +10,18 @@ module fc_mem_read (input logic clk, reset, enable,
             addr0 <= 9'b000000000;
             addr1 <= 9'b011000000;
             delay <= 4'b0000;
+            done <= 1'b0;
         end
         else if (enable == 1'b1 && done == 1'b0 && delay == 4'b0000) begin
             addr0 <= addr0 + 9'b000000001;
             addr1 <= addr1 + 9'b000000001;
         end
-        else begin
+        else if (enable == 1'b1) begin
             delay <= delay + 4'b0001;
         end
-    end
-
-    always_comb begin
-        if(addr1 == 9'b101111111)
+        if(addr1 == 9'b101111111) begin
             done = 1'b1;
+        end
     end
 
 endmodule
