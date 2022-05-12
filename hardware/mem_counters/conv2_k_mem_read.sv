9e1fd612f52e8f8e2ce47b4b646481cab0830616
rdk2132 <rdk2132@columbia.edu>
2022-05-12 16:09:44 -0400

    maybe?

--- hardware/mem_counters/conv2_k_mem_read.sv
+++ hardware/mem_counters/conv2_k_mem_read.sv
@@ -5,39 +5,17 @@ module conv2_k_mem_read (input logic clk, reset, enable,
                         output logic done);
 
     logic [3:0] delay;
-    logic [5:0] count;
-    logic [7:0] addrcount;
-    logic [7:0] offset;
 
-    always_ff @(negedge clk or posedge reset) begin
+    always_ff @(posedge clk or posedge reset) begin
         if (reset == 1'b1) begin
+            addr0 <= 8'b00000000;
+            addr1 <= 8'b01001011;
             delay <= 4'b0000;
-            addrcount <= 8'b00000000;
-            offset <= 8'b0000000;
-            count <= 6'b000000;
         end
         else if (enable == 1'b1 && done == 1'b0) begin
-
-            //when address reaches 24, count
-            if(addrcount == 8'd24) begin
-                addrcount <= 8'b00000000;
-                if(count == 6'd63) begin
-                    count <= 6'd0;
-                    if(offset == 8'b00000000) begin
-                        offset <= 8'd25;
-                    end
-                    else if(offset == 8'd25) begin
-                        offset <= 8'd50;
-                    end
-                end
-                else begin
-                    count <= count + 6'd1;
-                end
-            end
-
-            //else, increment by 1
-            else if(delay == 4'b0000) begin
-                addrcount <= addrcount + 8'b00000001;
+            if(delay == 4'b0000) begin
+                addr0 <= addr0 + 8'b00000001;
+                addr1 <= addr1 + 8'b00000001;
             end
             else begin
                 delay <= delay + 4'b0001;
@@ -46,13 +24,11 @@ module conv2_k_mem_read (input logic clk, reset, enable,
     end
 
     always_comb begin
-        if(offset == 8'd50 && count == 6'd63) begin
+        if(addr1 == 8'b100101011) begin
             done = 1'b1;
         end
         else begin
             done = 1'b0;
         end
-        addr0 = addrcount + offset;
-        addr1 = addrcount + 8'b01001011 + offset;
     end
 endmodule
