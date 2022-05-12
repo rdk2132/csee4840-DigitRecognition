fc48caabff0164a6f8d72c51e205dd0eb2cbbaa1
rdk2132 <rdk2132@columbia.edu>
2022-05-12 15:28:27 -0400

    conv1 mem fix

--- hardware/RAMs/conv1_mem.v
+++ hardware/RAMs/conv1_mem.v
@@ -6,16 +6,12 @@ module conv1_mem ( input [8:0] address_a, address_b,
 				   output reg [15:0] q_a, q_b);
 
 	reg [15:0] ram[511:0];
-  logic [8:0] prev_addra, pprev_addra;
-  logic [8:0] prev_addrb, pprev_addrb;
 	// Port A 
 	always @ (posedge clock)
 	begin
-    pprev_addra <= address_a;
-    prev_addra <= pprev_addra;
 		if (wren_a) 
 		begin
-			ram[prev_addra] <= data_a;
+			ram[address_a] <= data_a;
 			q_a <= data_a;
 		end
 		else 
@@ -27,11 +23,9 @@ module conv1_mem ( input [8:0] address_a, address_b,
 	// Port B 
 	always @ (posedge clock)
 	begin
-    pprev_addrb <= address_b;
-    prev_addrb <= pprev_addrb;
     if (wren_b) 
 		begin
-			ram[prev_addrb] <= data_b;
+			ram[address_b] <= data_b;
 			q_b <= data_b;
 		end
 		else 
